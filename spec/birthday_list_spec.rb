c222a2effc9c384ba2fae0fd68b5883dbafce0b7
Jonathan Andrews <jonathanandrews@Jonathans-MacBook-Pro.local>
2018-09-07 07:51:44 +0000

    #birthday_search should search list for a particular date

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,16 +1,26 @@
-require 'birthday_list'
+require 'birthday_list.rb'
+
 describe BirthdayList do
   it {is_expected.to respond_to(:add_birthday)}
 
   it "#add_birthday should store the birthday" do
-    expect(subject.add_birthday("James")).to eq(["James"])
+    expect(subject.add_birthday("James", "10/10/10")).to eq([{ name: "James", date: "10/10/10"}])
   end
 
   it {is_expected.to respond_to(:print_list)}
 
   it "#print_list should print out the birthday list" do
-    subject.add_birthday("James")
-    subject.add_birthday("Tom")
-    expect(subject.print_list).to eq("James\nTom\n")
+    subject.add_birthday("James", "03/03/03")
+    subject.add_birthday("Tom", "05/05/05")
+    expect(subject.print_list).to eq("James: 03/03/03\nTom: 05/05/05\n")
   end
+
+  it {is_expected.to respond_to(:birthday_search)}
+
+  it "#birthday_search should search list for a particular date" do
+    subject.add_birthday("James", "03/03/03")
+    subject.add_birthday("Tom", "05/05/05")
+    expect(subject.birthday_search("03/03/03")).to eq("It's James's birthday today!")
+  end
+
 end
