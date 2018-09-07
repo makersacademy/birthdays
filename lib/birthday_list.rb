2cd73783f69062cbb0dba88224834802aa31ef9f
Jonathan Andrews <jonathanandrews@Jonathans-MacBook-Pro.local>
2018-09-07 07:35:15 +0000

    added #print_list method that prints names

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,9 +1,20 @@
 
 class BirthdayList
+
   def initialize
     @list = []
   end
+
   def add_birthday(name)
     @list << name
   end
+
+  def print_list
+    string = ""
+    @list.each do |name|
+      string += "#{name}\n"
+    end
+    string
+  end
+
 end
