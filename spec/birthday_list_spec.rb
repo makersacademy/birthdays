18f1894b4ddfe783f7813e689c5f05750f9a1e1d
Jonathan Andrews <jonathanandrews@Jonathans-MacBook-Pro.local>
2018-09-07 07:50:39 +0000

    #print_list should print out the birthday list

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -3,6 +3,14 @@ describe BirthdayList do
   it {is_expected.to respond_to(:add_birthday)}
 
   it "#add_birthday should store the birthday" do
-    expect(BirthdayList.new.add_birthday("James")).to eq(["James"])
+    expect(subject.add_birthday("James")).to eq(["James"])
+  end
+
+  it {is_expected.to respond_to(:print_list)}
+
+  it "#print_list should print out the birthday list" do
+    subject.add_birthday("James")
+    subject.add_birthday("Tom")
+    expect(subject.print_list).to eq("James\nTom\n")
   end
 end
