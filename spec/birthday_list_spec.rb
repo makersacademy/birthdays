1010430bcafb2d2df49df394ed74853843dce955
Jonathan Andrews <jonathanandrews@Jonathans-MacBook-Pro.local>
2018-09-07 07:49:26 +0000

    #add_birthday should store the birthday

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,4 +1,8 @@
 require 'birthday_list'
 describe BirthdayList do
   it {is_expected.to respond_to(:add_birthday)}
+
+  it "#add_birthday should store the birthday" do
+    expect(BirthdayList.new.add_birthday("James")).to eq(["James"])
+  end
 end
