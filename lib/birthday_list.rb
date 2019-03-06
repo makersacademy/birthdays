24324f8f05a9a66365615ba622ae85d6bd0ee713
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-03-06 11:22:13 +0000

    Adds stores all birthdays requirement with tests

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,9 +1,20 @@
-def add_birthday(name, date)
+def birthdays_list
   @birthdays = Hash.new
+end
+
+def add_birthday(name, date)
   @birthdays[name] = date
   "Birthday added!"
 end
 
 def show_birthdays
-  return @birthdays
+  @birthdays
 end
+
+
+birthdays_list
+puts add_birthday("Matt", "31st Jan")
+puts add_birthday("Katie", "4th Jul")
+puts add_birthday("Mum", "16th Oct")
+
+puts show_birthdays
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -2,23 +2,27 @@ require "birthday_list"
 
 describe "The add_birthday method" do
   it "gives a message when birthday added" do
+    birthdays_list
     expect(add_birthday("Katie", "4th July")).to eq "Birthday added!"
   end
 
-  # it "stores details when birthday is added" do
-  #   # expect(add_birthday("Katie", "4th July")).to include("Katie")
-  #   expect(add_birthday("Katie", "4th July")).to change(@birthdays, :count).by(1)
-  # end
+  it "stores details when birthday is added" do
+    birthdays_list
+    add_birthday("Katie", "4th July")
+    expect(show_birthdays).to include("Katie" => "4th July")
+  end
 end
 
 describe "The show_birthday method" do
   it "checks birthdays is a hash" do
+    birthdays_list
     add_birthday("Katie", "4th July")
     expect(show_birthdays).to be_an_instance_of(Hash)
   end
 
-  it "checks hash is not empty when birthday added" do
+  it "show_birthdays is not empty when birthday added" do
+    birthdays_list
     add_birthday("Katie", "4th July")
-    expect(show_birthdays).to include("Katie" => "4th July")
+    expect(show_birthdays).not_to be_empty
   end
 end
