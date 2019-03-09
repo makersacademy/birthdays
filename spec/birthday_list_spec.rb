8b4cf5d4d3a66f5655a61c8364a66485590407b7
Cosmin <mirceacosmin@gmail.com>
2019-03-09 21:43:39 +0000

    refactored birthday_list_spec to add context for class responding to its methods

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -3,9 +3,10 @@ require 'birthday_list'
 RSpec.describe Birthdays do
 
   it { is_expected.to be_kind_of(Birthdays) }
-  # test the methods
-  it { is_expected.to respond_to(:add_birthday) }
-  it { is_expected.to respond_to(:show_all) }
-  it { is_expected.to respond_to(:match_birthday) }
-  it { is_expected.to respond_to(:age) }
+  context 'responds to these methods' do
+    it { is_expected.to respond_to(:add_birthday) }
+    it { is_expected.to respond_to(:show_all) }
+    it { is_expected.to respond_to(:match_birthday) }
+    it { is_expected.to respond_to(:age) }
+  end
 end
