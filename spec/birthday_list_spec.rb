700d02c1de5ea5283cf1f0375d6309a2427d3a75
Cosmin <mirceacosmin@gmail.com>
2019-03-09 23:02:01 +0000

    rubocop suggestions incorporated

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe Birthdays do
   it "shows returns the birthdays stored in the birthday list - #view_all not implemented yet" do
     mock_class = instance_double(Birthdays)
     allow(mock_class).to receive(:view_all).and_return("Name: John Adam | Birthday: 10 January 1795")
-    expect(mock_class.view_all).to eq( "Name: John Adam | Birthday: 10 January 1795")
+    expect(mock_class.view_all).to eq("Name: John Adam | Birthday: 10 January 1795")
   end
 
   it "shows all birthdays from the list in a nice format - #view_all implemented - mocked Friend class" do
@@ -34,23 +34,22 @@ RSpec.describe Birthdays do
   
 
   it "shows a matching birthday" do
-    friend1 = double(name: "John Adams", birthday: Time.new(1795,3,9))
+    friend1 = double(name: "John Adams", birthday: Time.new(1795, 3, 9))
     subject.store(friend1)
     expect(subject.check_birthday.first).to eq friend1
   end
 
   it "shows matching birthdays" do
-    friend1 = double(name: "John Adams", birthday: Time.new(1795,3,9))
-    friend2 = double(name: "John Adams", birthday: Time.new(1900,3,9))
+    friend1 = double(name: "John Adams", birthday: Time.new(1795, 3, 9))
+    friend2 = double(name: "John Adams", birthday: Time.new(1900, 3, 9))
     subject.store(friend1)
     subject.store(friend2)
     expect(subject.check_birthday).to eq [friend1, friend2]
   end
 
-
   context "can calculate the age of the matched person " do
     it "calculates the age" do
-      friend = double(name: "John Adams", birthday: Time.new(2000,3,9))
+      friend = double(name: "John Adams", birthday: Time.new(2000, 3, 9))
       subject.store(friend)
       expect(subject.age(friend)).to eq 19
     end 
