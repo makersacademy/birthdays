e17843604b3eea71e1e71881bd921f011e9b6c36
Cosmin <mirceacosmin@gmail.com>
2019-03-09 21:29:31 +0000

    created and passed test to check whether Birthdays class responds to :show_all

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,4 +1,7 @@
 class Birthdays
   def add_birthday
   end
+
+  def show_all
+  end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe Birthdays do
   it { is_expected.to be_kind_of(Birthdays) }
   # test the methods
   it { is_expected.to respond_to(:add_birthday)}
+  it { is_expected.to respond_to(:show_all) }
 end
