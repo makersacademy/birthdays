fcbd44584a0da3ddee11b428eb54f17f44e46911
Cosmin <mirceacosmin@gmail.com>
2019-03-10 09:19:09 +0000

    test Birthday class taking 3 arguments

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,2 +1,7 @@
 class Birthday
+  def initialize(first_name, second_name, birthday)
+    @first_name = first_name
+    @second_name = second_name
+    @birthday = birthday
+  end
 end
--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -1,4 +1,9 @@
 require 'birthday'
 RSpec.describe Birthday do 
+
+  #it { is_expected.to be_kind_of(Birthday)}
   
+  it 'takes 3 arguments: first_name, second_name, birthday' do
+    expect(Birthday).to respond_to(:new).with(3)
+  end
 end
