bf35ae93b1f31db2714f3235d7c72302f4433fd3
Cosmin <mirceacosmin@gmail.com>
2019-03-10 09:21:38 +0000

    tested and passed return of full name from birthday class

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,7 +1,12 @@
 class Birthday
+  attr_reader :first_name, :second_name, :birthday
   def initialize(first_name, second_name, birthday)
     @first_name = first_name
     @second_name = second_name
     @birthday = birthday
+  end
+
+  def name
+    "#{@first_name}  #{@second_name}"
   end
 end
--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Birthday do
   it 'takes 3 arguments: first_name, second_name, birthday' do
     expect(Birthday).to respond_to(:new).with(3)
   end
+
+  it 'returns a full name' do
+    birthday = Birthday.new("John","Adams","17 May 2000")
+    expect(birthday.name).to eq("#{birthday.first_name}  #{birthday.second_name}")
+  end
 end
