10d38dd3c165b9555148edad1c58ed416e1c83c4
Student <student@MAKERSs-MacBook-Pro-10.local>
2019-05-14 14:21:08 +0100

    created class and created method called add

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,4 @@
-
+class BirthdayList
+  def add
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,18 @@
+require 'birthday_list'
 
+
+describe BirthdayList do
+  it 'can create a list of birthdays' do
+  #Arrange/Act
+  birthdays = BirthdayList.new
+  #Assert
+  expect(birthdays).to be_a_kind_of(BirthdayList)
+end
+
+  it 'responds to a method called add' do
+    #Arrange/Act
+    birthdays = BirthdayList.new
+    #Assert
+    expect(birthdays).to respond_to(:add)
+  end
+end
