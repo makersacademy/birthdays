6c6d0912ed3a1d47d0c4938003b5576633ffd035
Student <student@MAKERSs-MacBook-Pro-10.local>
2019-05-14 14:32:42 +0100

    latest version

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -3,11 +3,11 @@ require 'birthday_list'
 
 describe BirthdayList do
   it 'can create a list of birthdays' do
-  #Arrange/Act
-  birthdays = BirthdayList.new
-  #Assert
-  expect(birthdays).to be_a_kind_of(BirthdayList)
-end
+    #Arrange/Act
+    birthdays = BirthdayList.new
+    #Assert
+    expect(birthdays).to be_a_kind_of(BirthdayList)
+  end
 
   it 'responds to a method called add' do
     #Arrange/Act
@@ -15,4 +15,12 @@ end
     #Assert
     expect(birthdays).to respond_to(:add)
   end
+
+  it 'can add a birthday to a list of birthdays' do
+    #Arrange
+    birthdays = BirthdayList.new
+    #Assert
+    expect(birthdays.add).to include("12.05.1995")
+  end
+
 end
