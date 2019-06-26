aaa9e8121d0b1aa424cc81ea39c2f206110a2410
timdavidcole <timdavidcole@gmail.com>
2019-06-26 13:40:23 +0100

    final commit

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,3 +1,67 @@
-def add_birthday
+require 'date'
+BirthDayList = Class.new
+
+class BirthDayList
+
+  def initialize
+    @birthday_array = []
+    @this_day = Time.new.day
+    @this_month = Time.new.month
+    @this_year = Time.new.year
+  end
+
+  def add(name, birthday)
+    @birthday_array << { name: name, birthday: birthday }
+  end
+
+  def show
+    @birthday_array.each do |friend|
+      puts "#{friend[:name]}: #{friend[:birthday]}"
+    end
+  end
+
+  def current_birthdays
+    @birthday_array.each do |friend|
+      birthday(friend)
+      age
+      puts_birthday(friend)
+    end
+  end
+
+  def age
+    if @bday_month <= @this_month && @bday_day <= @this_day
+      @age = @this_year - @bday_year
+    else
+      @age = @this_year - @bday_year - 1
+    end
+  end
+
+  def birthday(friend)
+    @bday_day = friend[:birthday].split('/').first.to_i
+    @bday_month = friend[:birthday].split('/')[1].to_i
+    @bday_year = friend[:birthday].split('/').last.to_i
+  end
+
+  def puts_birthday(friend)
+    birthday_message(friend) if day_true && month_true
+  end
+
+  def day_true
+    @bday_day == @this_day
+  end
+
+  def month_true
+    @bday_month == @this_month
+  end
+
+  def birthday_message(friend)
+    puts "Lucky #{friend[:name]}, today they're #{@age} years old!"
+  end
 
 end
+
+new_list = BirthDayList.new
+new_list.add("Tim Cole", "03/07/1985")
+new_list.add("Lucy Cole", "08/09/1983")
+new_list.add("Birthday Boy", "26/06/1984")
+new_list.current_birthdays
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,7 +1,37 @@
 require 'birthday_list.rb'
 
-describe "add_birthday" do
+describe "add a birthday" do
   it "adds birthday hash to array" do
-    expect(add_birthday("Tim Cole", "03/07/1985")).to eq [{name: "Tim Cole", birthday: "03/07/1985"}]
+    new_list = BirthDayList.new
+    expect(new_list.add("Tim Cole", "03/07/1985")).to eq [{ name: "Tim Cole", birthday: "03/07/1985" }]
+  end
+end
+
+describe "add 2 birthdays" do
+  it "adds 2nd birthday to array" do
+    new_list = BirthDayList.new
+    new_list.add("Tim Cole", "03/07/1985")
+    result = new_list.add("Lucy Cole", "08/09/1983")
+    expected = [{ name: "Tim Cole", birthday: "03/07/1985" }, { name: "Lucy Cole", birthday: "08/09/1983" }]
+    expect(result).to eq expected
+  end
+end
+
+describe "show list" do
+  it "displays list in pleasant format" do
+    new_list = BirthDayList.new
+    new_list.add("Tim Cole", "03/07/1985")
+    new_list.add("Lucy Cole", "08/09/1983")
+    expect { new_list.show }.to output("Tim Cole: 03/07/1985\nLucy Cole: 08/09/1983\n").to_stdout
+  end
+end
+
+describe "shows current birthdays" do
+  it "displays birthday message if today is birthday" do
+    new_list = BirthDayList.new
+    new_list.add("Tim Cole", "03/07/1985")
+    new_list.add("Lucy Cole", "08/09/1983")
+    new_list.add("Birthday Boy", "26/06/1984")
+    expect { new_list.current_birthdays }.to output("Lucky Birthday Boy, today they're 35 years old!\n").to_stdout
   end
 end
