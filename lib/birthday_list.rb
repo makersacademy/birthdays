a4962ee837b49ce1ea776c8ccc6875816c6b3854
Student <student@Makerss-MacBook-Pro.local>
2019-06-26 12:16:36 +0100

    code working fully, tidying rspec

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,13 +1,40 @@
 
-$birthdays = []
+$birthdays = {}
 
 def add_birthday(name,dob)
-  entry = {name => dob}
-  $birthdays.push(entry)
+  $birthdays[name] = dob
   return "Name stored!"
 end
 
 
 def check_birthdays
   return $birthdays
+end
+
+def how_old(name)
+  dob = $birthdays[name].split(' ').map(&:to_i)
+  require 'date'
+  date = Date.today.to_s.split('-').reverse.map(&:to_i)
+  birthdaygone = date[2] - dob[2]
+  notyetgone = date[2] - dob[2] - 1
+  if date[1] > dob[1]
+    birthdaygone
+  elsif date[1] < dob[1]
+    notyetgone
+  else
+    date[0] >= dob[0] ? birthdaygone : notyetgone
+  end
+end
+
+def whose_birthday
+  birthdaytoday = false
+  date = Date.today.to_s.split('-').reverse.map(&:to_i)
+  $birthdays.each { |name, dob|
+    dob = dob.split(' ').map(&:to_i)
+    if date[0] == dob[0] && date[1] == dob[1]
+      birthdaytoday = true
+      return "#{name} is #{how_old(name)} years old today."
+    end
+  }
+  return "nobody's bday ): " if birthdaytoday == false
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -3,12 +3,49 @@ require 'birthday_list'
 
 describe "the add birthday method" do
   it "returns a confirmation message" do
-    expect(add_birthday("Paul","27.04.1993")).to eq "Name stored!"
+    expect(add_birthday("Paul","27 04 1993")).to eq "Name stored!"
+  end
+  it "returns a confirmation message" do
+    expect(add_birthday("Zi","12 06 1991")).to eq "Name stored!"
+  end
+  it "returns a confirmation message" do
+    expect(add_birthday("Test","12 08 1991")).to eq "Name stored!"
+  end
+  it "returns a confirmation message" do
+    expect(add_birthday("Fake","26 06 1991")).to eq "Name stored!"
   end
 end
 
 describe "check birthdays method" do
+
   it "check contents of birthday array" do
-    expect(check_birthdays).to eq ([{"Paul"=>"27.04.1993"}])
+    $birthdays = {}
+    expect(check_birthdays).not_to include ({"Paul"=>"27 04 1993"})
+  end
+end
+
+describe "how old method" do
+  it "works out how old someone is" do
+    add_birthday("Paul","27 04 1993")
+    expect(how_old("Paul")).to eq 26
+  end
+
+end
+
+describe "how old method" do
+  it "works out how old someone is" do
+    add_birthday("Zi","12 06 1991")
+    expect(how_old("Zi")).to eq 28
+  end
+end
+
+describe "whose birthday method" do
+  it "tells me whose birthday is today" do
+    add_birthday("Fake","26 06 1991")
+    expect(whose_birthday).to eq "Fake is 28 years old today."
+  end
+  it "tells me if it's noone's bday" do
+    add_birthday("Fake","24 06 1991")
+    expect(whose_birthday).to eq "nobody's bday ): "
   end
 end
