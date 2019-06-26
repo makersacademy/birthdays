739f1b81cb7896ea7398bb9b94d000bf4b77e11f
Student <student@Makerss-MacBook-Pro.local>
2019-06-26 10:31:39 +0100

    added functions to add birthday and check current birthday list

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,13 @@
 
+$birthdays = []
+
+def add_birthday(name,dob)
+  entry = {name => dob}
+  $birthdays.push(entry)
+  return "Name stored!"
+end
+
+
+def check_birthdays
+  return $birthdays
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,14 @@
+require 'birthday_list'
 
+
+describe "the add birthday method" do
+  it "returns a confirmation message" do
+    expect(add_birthday("Paul","27.04.1993")).to eq "Name stored!"
+  end
+end
+
+describe "check birthdays method" do
+  it "check contents of birthday array" do
+    expect(check_birthdays).to eq ([{"Paul"=>"27.04.1993"}])
+  end
+end
