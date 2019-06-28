63d1484f5ef99dc8bb0055328aa1b5e8d3f07488
timdavidcole <timdavidcole@gmail.com>
2019-06-28 13:33:18 +0100

    Test-drove a new birthday class

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,6 +1,38 @@
 require 'date'
 BirthDayList = Class.new
 
+class Birthday
+
+  DATE_FORMAT = '%m-%d'
+
+  def age(friend)
+    if already_born(friend)
+      Time.now.year - Date.parse(friend[:birthday]).year
+    else
+      Time.now.year - Date.parse(friend[:birthday]).year - 1
+    end
+  end
+
+  def birthday?(friend)
+    today == birthday(friend)
+  end
+
+  private
+
+  def already_born(friend)
+    birthday(friend) <= Time.now.strftime(DATE_FORMAT)
+  end
+
+  def birthday(friend)
+    Date.parse(friend[:birthday]).strftime(DATE_FORMAT)
+  end
+
+  def today
+    Time.now.strftime(DATE_FORMAT)
+  end
+
+end
+
 class BirthDayList
 
   DATE_FORMAT = '%m-%d'
@@ -15,44 +47,21 @@ class BirthDayList
 
   def show
     @birthday_array.each do |friend|
+
       puts "#{friend[:name]}: #{friend[:birthday]}"
     end
   end
 
   def current_birthdays
     @birthday_array.each do |friend|
-      puts_birthday(friend)
+      birthday_message(friend) if Birthday.new.birthday?(friend)
     end
   end
 
-  private
-
-  def age(friend)
-    if already_born(friend)
-      Time.now.year - Date.parse(friend[:birthday]).year
-    else
-      Time.now.year - Date.parse(friend[:birthday]).year - 1
-    end
-  end
-
-  def already_born(friend)
-    birthday(friend) <= Time.now.strftime(DATE_FORMAT)
-  end
-
-  def birthday(friend)
-    Date.parse(friend[:birthday]).strftime(DATE_FORMAT)
-  end
-
-  def puts_birthday(friend)
-    birthday_message(friend) if birthday(friend) == today
-  end
+private
 
   def birthday_message(friend)
-    puts "Lucky #{friend[:name]}, today they're #{age(friend)} years old!"
-  end
-
-  def today
-    Time.now.strftime(DATE_FORMAT)
+    puts "Lucky #{friend[:name]}, today they're #{Birthday.new.age(friend)} years old!"
   end
 
 end
