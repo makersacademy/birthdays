c0ab57cb82bcd5b514ed0dd8a31468e2717acb7e
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-08-26 11:49:59 +0100

    Add tests and methods for adding to list and printing list

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,18 @@
+class BirthdayList
 
+  def initialize
+    @birthday_list = Hash.new
+  end
+
+  def add(name, dob)
+    @birthday_list[name] = dob
+    puts "#{name} has been added to the list!"
+  end
+
+  def print_list
+    @birthday_list.collect do |name, dob|
+      puts "#{name}: #{dob}"
+    end
+  end
+
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,21 @@
+require 'birthday_list'
 
+describe BirthdayList do
+  let(:name) { double :name }
+  let(:dob) { double :dob}
+
+  describe '#add' do
+    it 'confirms when new name and birthday added' do
+      expect { subject.add(name, dob) }.to output("#{name} has been added to the list!\n").to_stdout
+    end
+  end
+
+  describe '#print_list' do
+    it 'prints all names and birthdays' do
+      subject.add(name, dob)
+      expect { subject.print_list }.to output("#{name}: #{dob}\n").to_stdout
+    end
+  end
+
+
+end
