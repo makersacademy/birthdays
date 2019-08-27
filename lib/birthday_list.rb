2537828cef6bad71a1dbe57754318f1dc96d8ebf
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-08-27 21:58:01 +0100

    Add methods and tests for add to list and print list

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,18 +1,31 @@
+require 'date'
+
 class BirthdayList
 
   def initialize
     @birthday_list = Hash.new
   end
 
+  # input name and date of birth in the format of YYYY/MM/DD
   def add(name, dob)
-    @birthday_list[name] = dob
+    @birthday_list[name] = str_to_date(dob)
     puts "#{name} has been added to the list!"
   end
 
   def print_list
     @birthday_list.collect do |name, dob|
-      puts "#{name}: #{dob}"
+      puts "#{name}: #{date_to_str(dob)}"
     end
   end
 
+  private
+
+  def str_to_date(dob)
+    Date.strptime(dob, "%Y/%m/%d")
+  end
+
+  def date_to_str(dob)
+    dob.strftime("%Y/%m/%d")
+  end
+   
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,21 +1,35 @@
 require 'birthday_list'
+require 'date'
 
 describe BirthdayList do
-  let(:name) { double :name }
-  let(:dob) { double :dob}
-
+  
   describe '#add' do
     it 'confirms when new name and birthday added' do
-      expect { subject.add(name, dob) }.to output("#{name} has been added to the list!\n").to_stdout
+      name = "Harry Potter"
+      dob = "1980/7/31"
+
+      expect { subject.add(name, dob) }.to output(
+        "#{name} has been added to the list!\n").to_stdout  
     end
   end
 
   describe '#print_list' do
     it 'prints all names and birthdays' do
+      name = "Harry Potter"
+      dob = "1980/07/31"
+
       subject.add(name, dob)
       expect { subject.print_list }.to output("#{name}: #{dob}\n").to_stdout
     end
   end
 
+  
+  # decribe '#have_bithday_on' do
+  #   it 'checks whose birthday is today' do
+  #     TODO
+
+  # describe '#age' do
+  #   it 'calculates the age of people' do
+  #     TODO
 
 end
