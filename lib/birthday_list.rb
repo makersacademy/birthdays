abcc4628b493ad1b87a6af7c0c540f59d0330558
Alastair Gilles <agilles91@gmail.com>
2019-11-13 12:49:37 +0100

    Added #show method

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,7 +1,16 @@
 class BirthdayList
+  attr_reader :birthdays
+
+  def initialize
+    @birthdays = {}
+  end
+
   def store name, date
+    @birthdays[name] = date
   end
 
   def show
+    puts "Name".ljust(30) + "Birthday"
+    @birthdays.each { |name, b_day| puts name.ljust(30) + b_day }
   end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -4,6 +4,23 @@ describe BirthdayList do
   subject { described_class.new }
 
   it { is_expected.to be_instance_of BirthdayList }
+  it { is_expected.to have_attributes birthdays: {} }
   it { is_expected.to respond_to(:store).with(2).arguments }
   it { is_expected.to respond_to(:show).with(0).arguments }
+
+  describe "#store" do
+    it "should store the birthday in @birthdays" do
+      subject.store "Alastair", "01/12/1994"
+      is_expected.to have_attributes birthdays: {"Alastair" => "01/12/1994"}
+    end
+  end
+
+  describe "#show" do
+    it "should print formatted output" do
+      subject.store "Alastair", "01/12/1994"
+      expected_output = "Name".ljust(30) + "Birthday\n"
+      expected_output << "Alastair".ljust(30) + "01/12/1994\n"
+      expect{ subject.show }.to output(expected_output).to_stdout
+    end
+  end
 end
