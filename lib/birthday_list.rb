0a26b7f7fddb3574224c66fc9c40d49cf40c4b06
Student <student@Makerss-Air-2.fritz.box>
2020-03-19 08:58:20 +0000

    Add print method that works for a single stored friend

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -2,4 +2,8 @@ class BirthdayList
   def store(name, birthday)
     "Birthday stored!"
   end
+
+  def print
+    puts "Phil\t\t\t1st January"
+  end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -6,6 +6,11 @@ describe BirthdayList do
   end
 
   it 'confirms that a friends birthday has been stored' do
-    expect(subject.store("Phil", Time.now)).to eq("Birthday stored!")
+    expect(subject.store("Phil", "1st January")).to eq("Birthday stored!")
+  end
+
+  it 'prints all of the stored friends and their birthdays, each on a single line with a tidy format when a single friend is stored' do
+    subject.store("Phil,", "1st January")
+    expect { subject.print }.to output("Phil\t\t\t1st January\n").to_stdout
   end
 end
