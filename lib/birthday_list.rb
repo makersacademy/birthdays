81d4a302be93d3f2c3ed79063fde946af629ed6c
Student <student@Makerss-Air-2.fritz.box>
2020-03-19 08:45:58 +0000

    Make BirthdayList confirm when a birthday has been stored

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,5 +1,5 @@
 class BirthdayList
   def store(name, birthday)
-
+    "Birthday stored!"
   end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -4,4 +4,8 @@ describe BirthdayList do
   it 'stores friends birthdays' do
     expect(subject).to respond_to(:store).with(2).argument
   end
+
+  it 'confirms that a friends birthday has been stored' do
+    expect(subject.store("Phil", Time.now)).to eq("Birthday stored!")
+  end
 end
