f7869e72fef39d9270249fa71a118dac8cf1286e
Phil Vigus <philipvigus@yahoo.co.uk>
2020-03-20 09:15:53 +0000

    Override to_s in Birthday class
    to_s was overriden to return a 'pretty' version of the birthday's
    date.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,7 +1,11 @@
 class Birthday
   attr_reader :date
-  
+
   def initialize(day, month, year)
     @date = Time.new(year, month, day)
+  end
+
+  def to_s
+    "#{@date.day} #{@date.month} #{@date.year}"
   end
 end
