2bfbfcff0618b94c1f94f92e2fffde96d03bb411
Harry Turnbull <hturnbull@live.co.uk>
2020-03-20 19:38:30 +0000

    refactored tests to use doubles, so now #today will work on any day, and code is cleaner

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,8 +1,17 @@
 require 'birthday_list'
+require 'date'
 
 describe BirthdayTracker do
+  current_date = Date.today
+  date_tomorrow = Date.today + (60 * 60 * 24)
+  def age(dob)
+    current_year = Date.today.year
+    birth_year = Date.parse(dob).year
+    current_year - birth_year
+  end
 
-  subject { BirthdayTracker.new }
+  let(:always_bday) { double(:foo, name: 'Chris', dob: "1985-#{current_date.month}-#{current_date.day}") }
+  let(:never_bday) { double(:foo, name: 'Daisy', dob: "1995-#{date_tomorrow.month}-#{date_tomorrow.day}") }
 
   describe '#store' do
     it 'responds' do
@@ -24,13 +33,13 @@ describe BirthdayTracker do
       expect(subject).to respond_to :all
     end
     it 'prints a list of a stored friend and birthday' do
-      subject.store('Chris', '1985-03-20')
-      expect { subject.all }.to output("Chris: 1985-03-20\n").to_stdout
+      subject.store(always_bday.name, always_bday.dob)
+      expect { subject.all }.to output("#{always_bday.name}: #{always_bday.dob}\n").to_stdout
     end
     it 'prints two stored friends and birthdays' do
-      subject.store('Chris', '1985-03-20')
-      subject.store('Daisy', '1995-04-10')
-      expect { subject.all }.to output("Chris: 1985-03-20\nDaisy: 1995-04-10\n").to_stdout
+      subject.store(always_bday.name, always_bday.dob)
+      subject.store(never_bday.name, never_bday.dob)
+      expect { subject.all }.to output("#{always_bday.name}: #{always_bday.dob}\n#{never_bday.name}: #{never_bday.dob}\n").to_stdout
     end
   end
 
@@ -39,11 +48,9 @@ describe BirthdayTracker do
       expect(subject).to respond_to :today
     end
     it "prints a list of friends if their birthday is today" do
-      # Chris's bday is today
-      subject.store('Chris', '1985-03-20')
-      # Daisy's bday is not today
-      subject.store('Daisy', '1995-04-10')
-      expect { subject.today }.to output("It's Chris's birthday today! They are 35 years old!\n").to_stdout
+      subject.store(always_bday.name, always_bday.dob)
+      subject.store(never_bday.name, never_bday.dob)
+      expect { subject.today }.to output("It's #{always_bday.name}'s birthday today! They are #{age(always_bday.dob)} years old!\n").to_stdout
     end
   end
 
