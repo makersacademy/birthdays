93c2e30d96e7136e535f7016bd1b8fca930a3082
Phil Vigus <philipvigus@yahoo.co.uk>
2020-03-20 08:58:21 +0000

    Remove birthday_list_spec dependence on Birthday class
    Doubling and mocking were used to remove the BirthdayList test
    spec's dependence on the Birthday class.

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,24 +1,25 @@
 require 'birthday_list'
 
 describe BirthdayList do
+  let(:first_birthday) { double(:birthday, :date => "1st January") }
+  let(:second_birthday) { double(:birthday, :date => "15th August") }
+
   it 'stores friends birthdays' do
     expect(subject).to respond_to(:store).with(2).argument
   end
 
   it 'confirms that a friends birthday has been stored' do
-    birthday = Birthday.new("1st January")
-    expect(subject.store("Phil", birthday)).to eq("Birthday stored!")
+    expect(subject.store("Phil", first_birthday)).to eq("Birthday stored!")
   end
 
   it 'prints all of the stored friends and their birthdays, each on a single line with a tidy format when a single friend is stored' do
-    birthday = Birthday.new("1st January")
-    subject.store("Phil", birthday)
+    subject.store("Phil", first_birthday)
     expect { subject.print_stored_friends }.to output("Phil#{BirthdayList::COLUMN_SPACER}1st January\n").to_stdout
   end
 
   it 'prints all stored friends and their birthdays, each on a single line with a tidy format when multiple friends are stored' do
-    subject.store("Phil", Birthday.new("1st January"))
-    subject.store("Steve", Birthday.new("15th August"))
+    subject.store("Phil", first_birthday)
+    subject.store("Steve", second_birthday)
     expect { subject.print_stored_friends }.to output("Phil#{BirthdayList::COLUMN_SPACER}1st January\nSteve#{BirthdayList::COLUMN_SPACER}15th August\n").to_stdout
   end
 end
