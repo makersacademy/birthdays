7df2e9c9d02197d9c63d6ec4fa7b90b8d569f4c0
Phil Vigus <philipvigus@yahoo.co.uk>
2020-03-20 10:46:07 +0000

    Fix errors in birthday_list_spec
    The incorrect doubles were being used in the tests, which were not
    mocking the birthday class with the date as an instance of Time.

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -18,12 +18,26 @@ class BirthdayList
   end
 
   def print_todays_birthdays
-
+    today = Time.now
+    @friends_and_birthdays.each do |friend, birthday|
+      if is_birthday_today?(birthday, today)
+        friend_age = calculate_friend_age(birthday, today)
+        puts "It's #{friend}'s birthday today! They are #{friend_age} years old!"
+      end
+    end
   end
 
   private
 
   def print_friend_and_birthday(friend, birthday)
-    puts "#{friend}#{COLUMN_SPACER}#{birthday.date}"
+    puts "#{friend}#{COLUMN_SPACER}#{birthday.to_s}"
+  end
+
+  def is_birthday_today?(birthday, today)
+    birthday.date.month == today.month && birthday.date.day == today.day
+  end
+
+  def calculate_friend_age(birthday, today)
+    today.year - birthday.date.year
   end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,9 +1,8 @@
 require 'birthday_list'
 
 describe BirthdayList do
-  let(:first_birthday) { double(:birthday, :date => "1st January") }
-  let(:second_birthday) { double(:birthday, :date => "15th August") }
-  let(:first_birthday_new) {double(:birthday, :date => Time.new(1977, 1, 1), :to_s => "1 1 1977")}
+  let(:first_birthday) {double(:birthday, :date => Time.new(1977, 1, 1), :to_s => "1 1 1977")}
+  let(:second_birthday) {double(:birthday, :date => Time.new(1958, 8, 15), :to_s => "15 8 1958")}
 
   it 'stores friends birthdays' do
     expect(subject).to respond_to(:store).with(2).argument
@@ -15,17 +14,33 @@ describe BirthdayList do
 
   it 'prints all of the stored friends and their birthdays, each on a single line with a tidy format when a single friend is stored' do
     subject.store("Phil", first_birthday)
-    expect { subject.print_stored_friends }.to output("Phil#{BirthdayList::COLUMN_SPACER}1st January\n").to_stdout
+    expect { subject.print_stored_friends }.to output("Phil#{BirthdayList::COLUMN_SPACER}1 1 1977\n").to_stdout
   end
 
   it 'prints all stored friends and their birthdays, each on a single line with a tidy format when multiple friends are stored' do
     subject.store("Phil", first_birthday)
     subject.store("Steve", second_birthday)
-    expect { subject.print_stored_friends }.to output("Phil#{BirthdayList::COLUMN_SPACER}1st January\nSteve#{BirthdayList::COLUMN_SPACER}15th August\n").to_stdout
+    expect { subject.print_stored_friends }.to output("Phil#{BirthdayList::COLUMN_SPACER}1 1 1977\nSteve#{BirthdayList::COLUMN_SPACER}15 8 1958\n").to_stdout
   end
 
   it 'when you ask it to print todays birthdays and there are none, it prints nothing' do
     subject.store("Phil", first_birthday)
     expect { subject.print_todays_birthdays }.to output("").to_stdout
+  end
+
+  it 'when you ask it to print todays birthdays and there are some, it prints them' do
+    current_month = Time.now.month
+    current_date = Time.now.day
+
+    birthday_today_1 = double(:birthday, :date => Time.new(1956, current_month, current_date), :to_s => "#{current_date} #{current_month} #{1956}")
+    birthday_today_2 = double(:birthday, :date => Time.new(1984, current_month, current_date), :to_s => "#{current_date} #{current_month} #{1984}")
+    
+    subject.store("Phil", birthday_today_1)
+    subject.store("Emma", first_birthday)
+    subject.store("Steve", birthday_today_2)
+
+    expected_output = "It's Phil's birthday today! They are #{Time.now.year - 1956} years old!\n"
+    expected_output << "It's Steve's birthday today! They are #{Time.now.year - 1984} years old!\n"
+    expect { subject.print_todays_birthdays }.to output(expected_output).to_stdout
   end
 end
