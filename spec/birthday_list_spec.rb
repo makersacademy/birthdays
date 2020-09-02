2fbcb99639f25a8b281339a514647bf29417cfea
Connor Worthington <worthington.connor@gmail.com>
2020-09-02 13:19:57 +0100

    added first test

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,7 @@
+require 'birthday_list'
 
+describe 'store birthdays' do
+  it 'stores the name and birthday' do
+    expect(add("Connor", "October")).to eq [{:name => "Connor", :birthday => "October"}]
+  end 
+end
