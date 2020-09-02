45ef645bfd34aaa51062342f6f6550cf27e47506
Veronica Lee <veronicavlee@icloud.com>
2020-09-02 22:22:44 +0100

    Update store_birthday test to pass in a name and birthday

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,8 +1,8 @@
 require 'birthday_list'
 
-describe "store_birthday method" do
-  it "returns a success message" do
-    expect(store_birthday).to eq "Birthday stored!"
+describe 'store_birthday method' do
+  it 'returns a success message when passed a name and birthday' do
+    expect(store_birthday('Jim', 01-01-1945)).to eq 'Birthday stored!'
   end
 end
 
