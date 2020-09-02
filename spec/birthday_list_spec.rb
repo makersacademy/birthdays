e855318e59c9e57a2071ce4c8d0303d20384fe9b
Veronica Lee <veronicavlee@icloud.com>
2020-09-02 23:01:15 +0100

    Add test for print_birthdays that expects a single friend and birthday to be printed

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -6,3 +6,11 @@ describe 'store_birthday method' do
   end
 end
 
+describe 'print_birthdays method' do
+  it 'prints a list of stored friends and their birthdays' do
+    name = 'Jim'
+    birthday = 01-01-1945
+    store_birthday(name, birthday)
+    expect(get_birthdays).to eq "#{name} #{birthday}"
+  end
+end
