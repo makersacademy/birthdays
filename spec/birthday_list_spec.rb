6a9829f5e5afc37488b2f6c3096c53a58cf7778c
Veronica Lee <veronicavlee@icloud.com>
2020-09-03 13:00:06 +0100

    Fix tests to pass in date argument as string

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -5,19 +5,19 @@ describe BirthdayList do
 
   describe 'store_birthday method' do
     it 'returns a success message when passed a name and birthday' do
-      expect(subject.store_birthday('Jim', 01-01-1945)).to eq 'Birthday stored!'
+      expect(subject.store_birthday('Jim', '01-01-1945')).to eq 'Birthday stored!'
     end
   end
 
   describe 'print_birthdays method' do
     it 'prints a birthday in a tidy format, when only one saved' do
-      subject.store_birthday('Veronica Lee', 01-01-1960)
+      subject.store_birthday('Veronica Lee', '01-01-1960')
       expect { subject.print_birthdays }.to output("Veronica Lee (1 January 1960)\n").to_stdout
     end
 
     it 'prints all stored birthdays, each on a line in a tidy format' do
-      subject.store_birthday('Veronica Lee', 01-01-1960)
-      subject.store_birthday('Harry Potter', 31-07-1980)
+      subject.store_birthday('Veronica Lee', '01-01-1960')
+      subject.store_birthday('Harry Potter', '31-07-1980')
       expect { subject.print_birthdays }.to output("Veronica Lee (1 January 1960)\nHarry Potter (31 July 1980)").to_stdout
     end
   end
