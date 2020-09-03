6c1faa4cc072b6d64d22be77c0f9f4e414ae1de0
ollie beney <ombeney@gmail.com>
2020-09-03 12:36:33 +0100

    program works to spec

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,34 @@
+require 'date'
+require 'timecop'
+class Birthdays 
 
+  def initialize
+    @birthday_list = Array.new
+  end
+
+  def add(name, date)
+    @birthday_list.push({ name: name, birthday: date })
+  end
+
+  def show
+   @birthday_list.each { |person| puts person }
+  end
+
+  def check
+    this_year = Time.now.strftime("%Y").to_i
+    @birthday_list.each {|person|
+      birth_year = person[:birthday].split('').last(4).join("").to_i
+      age = this_year - birth_year
+      birthday = person[:birthday].split("").first(5).join
+      today = Time.now.strftime("%d/%m")
+      if birthday == today
+        return "it is #{person[:name]}\'s birthday today, they are #{age} years old!"
+      end
+                          }
+  end
+  
+  
+
+
+
+end
