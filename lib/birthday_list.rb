2be99cd08fb73461c6aea1fe52a26e158eef2606
Connor Worthington <worthington.connor@gmail.com>
2020-09-03 11:59:16 +0100

    adds Birthday class

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,5 +1,10 @@
-def add(name, birthday)
-  birthday_list = []
-  birthday_list << ({:name => name, :birthday => birthday})
-  birthday_list
+class Birthday
+
+@birthday_list = []
+
+  def self.add(name, birthday)
+    @birthday_list << {:name => name, :birthday => birthday}
+    @birthday_list
+  end
+
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,7 +1,11 @@
 require 'birthday_list'
 
-describe 'store birthdays' do
+describe Birthday do
   it 'stores the name and birthday' do
-    expect(add("Connor", "October")).to eq [{:name => "Connor", :birthday => "October"}]
-  end 
+    expect(Birthday.add("Connor", "October")).to eq [{:name => "Connor", :birthday => "October"}]
+  end
+
+  it "stores name and birthday" do
+    expect(Birthday.add("Michael", "April")).to eq [{:name => "Connor", :birthday => "October"}, {:name => "Michael", :birthday => "April"}]
+  end
 end
