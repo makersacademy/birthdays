006caf1d920e4253e1128f32d36d8de2ea7f0736
Veronica Lee <veronicavlee@icloud.com>
2020-09-03 18:43:37 +0100

    Change date format that birthdays are stored in to full format

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -5,14 +5,13 @@ class BirthdayList
   @birthday_list = []
 
   def self.store_birthday(name, date)
-    birthday = { name: name, date: Date.strptime(date, '%d-%m-%Y') }
+    birthday = { name: name, date: Date.strptime(date, '%d-%m-%Y').strftime('%-d %B %Y') }
     @birthday_list << birthday
-    "Birthday stored!"
   end
 
   def self.print_birthdays
     @birthday_list.each do | birthday |
-      puts "#{birthday[:name]} (#{birthday[:date].strftime('%-d %B %Y')})"
+      puts "#{birthday[:name]} (#{birthday[:date]})"
     end
   end
 
