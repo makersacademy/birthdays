e39f5105374041620ad0e751bdf698f05a4655f4
Connor Worthington <worthington.connor@gmail.com>
2020-09-04 10:19:16 +0100

    add method for checking if there are any birthdays on current day

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,9 +1,9 @@
 class Birthday
 
   def initialize
-    @birthday_list = []
+   @birthday_list = []
   end
-   
+
   def add(name, birthday)
     @birthday_list << {:name => name, :birthday => birthday}
     @birthday_list
@@ -12,6 +12,16 @@ class Birthday
   def print_list
     @birthday_list.each do |person|
       puts "#{person[:name]}, #{person[:birthday]}"
+    end
+  end
+
+  def today
+    @birthday_list.each do |person|
+      date = Time.new
+      today = date.strftime("%d %B")
+      if person[:birthday] == today
+        print "Today is #{person[:name]}'s birthday!"
+      end
     end
   end
 
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -19,4 +19,13 @@ describe Birthday do
     birthday_list.add("Michael", "April")
     expect{birthday_list.print_list}.to output("Connor, October\nMichael, April\n").to_stdout
   end
+
+  it "finds which dates in the birthday_list array match todays date" do
+    birthday_list = Birthday.new
+    birthday_list.add("Connor", "3 October")
+    birthday_list.add("James", "04 September")
+    expect{birthday_list.today}.to output("Today is James's birthday!").to_stdout
+  end
+
+
 end
