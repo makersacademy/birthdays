d7865b86d2fd3d80da7c7864714069f96c95f877
Alex Withington-Smith <alex@awithsmith.com>
2020-10-28 13:32:04 +0000

    added first test for a Person class with a name and a DOB

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,8 @@
+require 'birthday_list'
 
+describe Person do
+  it "correctly initalises with a name and a date of birth" do
+    person1 = Person.new("Dave", Time.new(1991, 06, 04))
+    expect(person1).to be_a_type_of(Person)
+  end
+end
