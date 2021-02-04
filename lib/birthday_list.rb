7acf21fae7dc936b87ec7d33faacb0aeb6f8bbed
Christine Chee <christinechee88@hotmail.com>
2021-02-04 21:57:46 +0000

    Add view and check methods for birthday list class

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,27 @@
+# frozen_string_literal: true
+require 'date'
 
+class BirthdayList
+  def initialize
+    @list = []
+  end
+
+  def add(name, date)
+    @list << [name, date]
+  end
+
+  def view
+    @list.each do |x|
+      puts "Name: #{x[0]}, Birthday: #{x[1]}"
+    end
+  end
+
+  def check
+    @list.each do |x|
+      date = Date.parse(x[1].to_s)
+      if date.strftime('%d %b %Y') == Date.today.strftime('%d %b %Y')
+        puts "It's #{x[0]}'s birthday today! #{x[0]} is 100 years old!"
+      end
+    end
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,30 @@
+# frozen_string_literal: true
 
+require 'birthday_list'
+require 'date'
+
+describe BirthdayList do
+  it 'responds to adding a birthday' do
+    expect(subject).to respond_to(:add)
+  end
+  it 'adds a birthday and stores it' do
+    expect(subject.add('Annie', '23 Dec 1988')).to eq([['Annie', '23 Dec 1988']])
+  end
+
+  it 'adds more than one birthday' do
+    subject.add('Annie', '23 Dec 1988')
+    expect(subject.add('Bob','25 May 1991')).to eq([["Annie", "23 Dec 1988"], ["Bob", "25 May 1991"]])
+  end
+
+  it 'prints each birthday on a line in a tidy format' do
+    subject.add('Annie', '23 Dec 1988')
+    subject.add('Bob','25 May 1991')
+    expect{subject.view}.to output("Name: Annie, Birthday: 23 Dec 1988\nName: Bob, Birthday: 25 May 1991\n").to_stdout
+  end
+
+  it "checks if today is someone's birthday" do
+    subject.add('Bob', '25 May 1991')
+    subject.add('Moo', Date.today)
+    expect{subject.check}.to output("It's Moo's birthday today! Moo is 100 years old!\n").to_stdout
+  end
+end
