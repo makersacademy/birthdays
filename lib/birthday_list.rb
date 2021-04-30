879817594cbf290f7282c2d146daaebc81536269
Arif Ebrahim <>
2021-04-30 11:19:35 +0100

    completed user story 2

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,41 @@
+=begin
+  
+Requirements
+I want a program that I can load in IRB that allows me to:
+- Store all of my friends’ birthdays so I can keep track of them
+- See them all at once with their names and birthdays each on a line in a tidy format
+- Check whose birthday it is today - the program can check through the birthdays I have 
+stored and check each one to see if it’s someone’s birthday, and tells me something 
+like "It's Mary Poppin's birthday today! They are 124 years old!" - otherwise it 
+won't say anything.
+
+User Story:
+As a user,
+So that i can keep track of my friends' birthdays
+I want to store their birthdays
+
+As a user,
+So that i can see upcoming birthdays 
+I want to see them all at once with their name and birthday on a line
+  
+=end
+
+class Birthday
+  attr_reader :store
+
+  def initialize
+    @store = []
+  end
+
+  def save_birthday(name, date)
+    @store << {name: name, date: date}
+  end
+
+  def print_list
+    display_array = []
+    @store.each { |entry| display_array << "Name: #{entry[:name]}, Date: #{entry[:date]}" }
+    display_array.join("\n")
+  end
+
+end
 
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,18 @@
+require 'birthday_list'
 
+RSpec.describe Birthday do
+  describe "#save_birthday" do
+    it "should store a user entered birthday" do
+      subject.save_birthday("Bob", "01/04/1980")
+      expect(subject.store.length).to eq(1)
+    end
+  end
+
+  describe "#print_list" do
+    it "prints all entries" do
+      subject.save_birthday("Bob", "01/04/1980")
+      # subject.save_birthday("Ahmed", "22/06/1985")
+      expect(subject.print_list).to eq("Name: Bob, Date: 01/04/1980")
+    end
+  end
+end
