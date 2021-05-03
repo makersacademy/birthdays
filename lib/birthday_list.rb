2f0730cdd8e8c236b692c1c61060e9559680d33b
Arif Ebrahim <>
2021-05-03 09:40:02 +0100

    add tests pass

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -9,16 +9,24 @@ stored and check each one to see if it’s someone’s birthday, and tells me so
 like "It's Mary Poppin's birthday today! They are 124 years old!" - otherwise it 
 won't say anything.
 
-User Story:
+User Story 1:
 As a user,
 So that i can keep track of my friends' birthdays
 I want to store their birthdays
 
+User Story 2:
 As a user,
 So that i can see upcoming birthdays 
 I want to see them all at once with their name and birthday on a line
+
+User Story 3:
+As a user,
+So that i can whose birthday it is today,
+I want to check my list and get a message if it's someone's birthday
   
 =end
+
+require 'time'
 
 class Birthday
   attr_reader :store
@@ -35,6 +43,24 @@ class Birthday
     display_array = []
     @store.each { |entry| display_array << "Name: #{entry[:name]}, Date: #{entry[:date]}" }
     display_array.join("\n")
+  end
+
+  def check
+  # get todays date
+  date_string = Time.now.strftime("%d/%m")
+
+  # compare with dates in store
+  store_matches =  @store.select { |entry| entry[:date][0..4] == date_string }
+
+  #create display strings 
+  display_array = []
+  store_matches.each do |entry|
+    age = (Time.new.year - entry[:date][6..9].to_i)
+    display_array << "It's #{entry[:name]}'s birthday today! They are #{age} years old!"
+    end
+
+  # output matches
+  display_array.join("\n")
   end
 
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -9,10 +9,24 @@ RSpec.describe Birthday do
   end
 
   describe "#print_list" do
-    it "prints all entries" do
+    it "prints an entry" do
       subject.save_birthday("Bob", "01/04/1980")
-      # subject.save_birthday("Ahmed", "22/06/1985")
       expect(subject.print_list).to eq("Name: Bob, Date: 01/04/1980")
+    end
+  end
+
+  describe "#print_list" do
+    it "prints multiple entries" do
+      subject.save_birthday("Bob", "01/04/1980")
+      subject.save_birthday("Ahmed", "22/06/1985")
+      expect(subject.print_list).to eq("Name: Bob, Date: 01/04/1980\nName: Ahmed, Date: 22/06/1985")
+    end
+  end
+
+  describe "#check_list" do
+    it "should compare all entries with todays date and output if there is a match" do
+      subject.save_birthday("Mary Poppins", "03/05/1897")
+      expect(subject.check).to eq("It's Mary Poppins's birthday today! They are 124 years old!")
     end
   end
 end
