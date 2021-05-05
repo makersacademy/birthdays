72871b218686b83df3522931690ed1817581280b
IliasMariosG <57366310+IliasMariosG@users.noreply.github.com>
2021-05-05 22:33:20 +0300

    User sees them all at once with their names and birthdays in a tidy format

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -6,4 +6,10 @@ class BirthdayList
   def add(name, birthday)
     @list << { name: name, birthday: birthday }
   end
+
+  def show
+    @list.each {
+      |friend_bday| puts "#{friend_bday[:name]}, #{friend_bday[:birthday]}"
+    }
+  end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -15,4 +15,28 @@ describe BirthdayList do
     birthday_list.add('James', '23/08/1993')
     expect(birthday_list.add('Maria', '05/10/1975')).to eq([{ name: 'James', birthday: '23/08/1993' }, { name: 'Maria', birthday: '05/10/1975' }])
   end
+  describe '#show' do
+    it 'shows one friend at once on a line' do
+      birthday_list = BirthdayList.new
+
+      name = 'James'
+      birthday = '23/08/1993'
+
+      birthday_list.add(name, birthday)
+      expect { birthday_list.show }.to output("James, 23/08/1993\n").to_stdout
+    end
+    it 'shows two friends at once on a line' do
+      birthday_list = BirthdayList.new
+
+      name_james = 'James'
+      bday_james = '23/08/1993'
+      name_maria = 'Maria'
+      bday_maria = '10/12/1945'
+
+      birthday_list.add(name_james, bday_james)
+      birthday_list.add(name_maria, bday_maria)
+
+      expect { birthday_list.show }.to output("#{name_james}, #{bday_james}\n#{name_maria}, #{bday_maria}\n").to_stdout
+    end
+  end
 end
