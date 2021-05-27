53306eb014b486620f6f8658de7dcbc142e0442e
Cynthia Fu <yinnycfu@gmail.com>
2021-05-27 21:24:40 +0100

    unit test for showing birthdays

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -2,4 +2,9 @@ class Birthday
   def store(name, birthday)
     "Birthday stored!"
   end
+
+  def show
+    puts "Andrew's birthday is on 1st January"
+    puts "Ben's birthday is on 2nd January"
+  end
 end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -2,6 +2,15 @@ require 'birthday_list'
 
 describe Birthday do
   it 'can store birthdays' do
-    expect(subject.store("Andrew", "1st January 1995")).to eq "Birthday stored!"
+    expect(subject.store("Andrew", "1st January")).to eq "Birthday stored!"
   end
+
+  it 'can store another birthday' do
+    expect(subject.store("Ben", "2nd January")).to eq "Birthday stored!"
+  end
+
+  it 'can show the list of birthdays' do
+    expect { subject.show } .to output("Andrew's birthday is on 1st January\nBen's birthday is on 2nd January\n").to_stdout
+  end
+
 end
