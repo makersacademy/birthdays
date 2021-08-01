e7786935adcf92f6cfd094628e4bc303935f88ed
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-08-01 15:11:35 +0100

    Added BirthdayList class with add method

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,12 @@
+class BirthdayList
+  attr_reader :all
 
+  def initialize
+    @all = Hash.new { |h, k| h[k] = [] }
+  end
+  
+  def add birthday
+    @all[birthday.date] << birthday.name
+    return "Added birthday for #{birthday.name}"
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,19 @@
+require 'birthday_list'
+require 'date'
 
+describe BirthdayList do
+  it 'should return a list of birthdays' do
+    expect(subject.all).to be_instance_of(Hash)
+  end
+
+  it 'should be able to add birthdays' do
+    new_birthday = double('Birthday')
+
+    allow(new_birthday).to receive(:name) { 'Victoria' }
+    allow(new_birthday).to receive(:date) { Date.new(1982, 6, 3) }
+
+    subject.add new_birthday
+
+    expect(subject.all).to eq({ new_birthday.date => [new_birthday.name] })
+  end
+end
