7549003a026345572796549d32be9b43841b4a35
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2021-11-23 21:47:39 +0000

    Added BirthdayList Object and unit-test

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,11 @@
+require 'birthday_list'
 
+describe BirthdayList do
+	subject {BirthdayList.new}
+
+	context "initialization"
+	it 'creates an instance of itself' do
+		expect(subject).to be_an_instance_of(BirthdayList)
+	end
+
+end
