c1e096af229f862fd29a5877a8460b45dc2103c8
Paul <pkilgarriff@gmail.com>
2022-03-02 23:01:34 +0000

    Update rubocop.yml to use up-to-date config  - then make formatting corrections based on suggestions

--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -8,7 +8,8 @@ describe BirthdayList do
       :birthday,
       :name => "Mary Poppins",
       :birthdate => Date.new(1899,8,9)
-    ) }
+    )
+  }  
 
   it 'stores a birthday' do
     expect { list.add(birthday) }.to change { list.size }
@@ -16,7 +17,7 @@ describe BirthdayList do
 
   it 'prints a list of birthdays' do
     list.add(birthday)
-    expect{ list.print_all_birthdays }.to output("Mary Poppins - 09/08\n").to_stdout
+    expect { list.print_all_birthdays }.to output("Mary Poppins - 09/08\n").to_stdout
   end
 
   it 'produces a birthday reminder' do
