01a742112960bba786d0cd716a4e9999ebcbaea7
Paul <pkilgarriff@gmail.com>
2022-03-02 11:50:03 +0000

    Create BirthdayList class  - add methods    - initialize    - add(birthday)    - print_all_birthdays  - add attr_reader for birthdays  - test storing a birthday  - test printing all birthdays

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,26 @@
+# I want a program that I can load in IRB that allows me to
+# - Store all of my friends’ birthdays so I can keep track of them
+  # maintains data structure of names and birthdates
+# - See them all at once with their names and birthdays each on a line in a tidy format
+  # prints all birthdays in pretty format
+# - Check whose birthday it is today - the program can check through the birthdays I have stored and check each one to see if it’s someone’s birthday, and tells me something like "It's Mary Poppin's birthday today! They are 124 years old!" - otherwise it won't say anything
+  # registers the date today
+  # compares against a stored record
+  # returns a formatted string of name and age
+    # needs to calculate age
 
+class BirthdayList
+  attr_reader :birthdays
+  
+  def initialize
+    @birthdays = []
+  end
+
+  def add(birthday)
+    @birthdays << birthday
+  end
+
+  def print_all_birthdays
+    puts @birthdays
+  end
+end
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,16 @@
+require 'birthday_list'
 
+describe BirthdayList do
+  it 'stores a birthday' do
+    list = BirthdayList.new
+    birthday = "today"
+    expect { list.add(birthday) }.to change { list.birthdays }
+  end
+
+  it 'prints a list of birthdays' do
+    list = BirthdayList.new
+    birthday = "today"
+    list.add(birthday)
+    expect{ list.print_all_birthdays }.to output("today\n").to_stdout
+  end
+end
